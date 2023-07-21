20d52dc31c61cc689719401e0549bc86a087040a
maiconeduardo <maicon.eduardo@outlook.com>
2023-07-21 16:34:24 -0300

    Funcion

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-document.querySelector("#robotron").addEventListener("click")
+const robotron = document.querySelector("#robotron");
+/* função anonima com seta => ou pode ser escrito function antes do ()*/
+robotron.addEventListener("click", () => {
+
+    console.log("Cliquei no robo");
+})
+
+function dizOi(nome) {
+    console.log("Olá " + nome);
+    console.log("brabo");
+}
+
+dizOi("maicon");
